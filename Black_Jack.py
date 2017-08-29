b76d89784b93e2cfa2515bb5ddfa3ea589d31fa6
Joe <joeshan111@gmail.com>
2017-08-29 18:39:49 +0800

    Update Black_Jack.py

--- Black_Jack.py
+++ Black_Jack.py
@@ -6,6 +6,7 @@ class player:
     def __str__(self):
         return 'Play %s has %s chips.' % (self.name, str(self.chip))
 
+    
 class card:
     def __init__(self, suit, rank):
         self.suit = suit
@@ -13,6 +14,63 @@ class card:
     
     def __str__(self):
         return self.suit + self.rank
+    
+    
+class decks:
+    suits = ['Heart ', 'Diamond ','Club ','Spade ']
+    ranks = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+    
+    def __init__(self, deck_num):
+        self.card_pool = []
+        self.deck_num = deck_num
+        
+        for i in decks.suits:
+            for j in decks.ranks:
+                self.card_pool.append(card(i,j))
+        self.card_pool *= self.deck_num 
+    
+    def __str__(self):
+        return '%d decks shuffled together. %d cards left.' % (self.deck_num, len(self.card_pool)) 
+
+    def shuffle(self):
+        random.shuffle(self.card_pool)
+        
+    def draw_card(self):
+        return self.card_pool.pop()
+
+
+class hand:
+    def __init__(self):
+        self.hand_cards = []
+        self.value = 0
+        self.aces = 0
+    
+    def add_card(self, new_card):
+        self.hand_cards.append(new_card)
+        
+        if new_card.rank == 'A':
+            self.aces += 1
+    
+    def show_hand(self, hidden):
+        if hidden == True:
+            print('Hidden card')
+            for c in self.hand_cards[1:]:
+                print(c)
+        else:
+            for c in self.hand_cards:
+                print(c)
+    
+    def cal_value(self):
+        self.value = 0
+        
+        for i in self.hand_cards:
+            self.value += card_value[i.rank]
+        
+        if self.aces >0 and self.value <= 11:
+            self.value += 10
+            
+        return self.value
+
     
 def join_game(players):
     join = True
@@ -31,8 +89,38 @@ def join_game(players):
             print('\nPlayers in the game now:')
             for i in players:
                 print(i)
+    
+def make_bet(players):
+    for player in players:
+        while True:  
+            bet_tmp = input('%s, how much would you like to bet? ' % player.name)
+            type(bet_tmp)
+            bet_tmp
+            try:
+                if 0 < int(bet_tmp) <= int(player.chip):
+                    bet[player.name] = int(bet_tmp)
+                    break
+                else:
+                    print('Invalid bet!a')
+            except:
+                print('Invalid bet!b')
+                
+    print('\nFinished betting')
+    for player in players:
+        print('%s bet %d chips.' % (player.name, bet[player.name]))
+        
 
-                
-                
-players = []
-join_game(players)
+        
+def initialization():
+    import random         
+    global players, card_value, bet
+    players = [] # initialization
+    card_value = {'A':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'J':10,'Q':10,'K':10}
+    bet = {}
+    
+    join_game(players)
+    make_bet(players)
+
+    
+# test
+initialization()
